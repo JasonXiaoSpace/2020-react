9dacc4c1c0829d780b516e085f0244b0685afede
xiaojianjun <xiaojianjun@kuaishou.com>
2020-10-07 11:42:58 +0800

    after build

---
+++ build/precache-manifest.6d64cf9413a8b6502bade1fba48db46d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "570997d738429815c0ae944ef0bff4da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d4cf357836afd6cdf73",
+    "url": "/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d4cf357836afd6cdf73",
+    "url": "/static/js/main.c84ca712.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "1e3e4fa7308a6a36023ebd10e3c087ba",
+    "url": "/static/media/App.1e3e4fa7.less"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.less';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className="app">
+      <div className="outer">
+        <div className='container'>
+          <img src={logo} className="app-logo" alt="logo" />
+          <div className='mask'></div>
+        </div>
+
+        <p className="app-name">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="app-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
-      </header>
+      </div>
     </div>
   );
 }
