f3e4e1aa8587e070e6c52ca13082618eae670fbe
xiaojianjun <xiaojianjun@kuaishou.com>
2020-10-07 13:08:53 +0800

    build

---
+++ build/precache-manifest.a7db428b92c8ccc9bb23573d12f10fa4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "980bd1564cb1faca2059af7878137b88",
+    "url": "/2020-react/build/index.html"
+  },
+  {
+    "revision": "d0843694e6947f9a4276",
+    "url": "/2020-react/build/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/2020-react/build/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2020-react/build/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0843694e6947f9a4276",
+    "url": "/2020-react/build/static/js/main.f3941d2a.chunk.js"
+  },
+  {
+    "revision": "41cb13e70217feffc1c3",
+    "url": "/2020-react/build/static/js/runtime-main.7ca42c7b.js"
+  },
+  {
+    "revision": "1e3e4fa7308a6a36023ebd10e3c087ba",
+    "url": "/2020-react/build/static/media/App.1e3e4fa7.less"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/2020-react/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
